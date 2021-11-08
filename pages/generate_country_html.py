e6dfa6803ff9240fb44c6e8f8782218014f8ca2c
sofiahafner <sofia.f.hafner@gmail.com>
2021-11-07 19:36:25 -0500

    fixed some links on map

--- pages/generate_country_html.py
+++ pages/generate_country_html.py
@@ -1,7 +1,7 @@
 countries = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola","Antigua & Deps","Argentina","Armenia","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh",
 "Barbados","Belarus","Belgium","Belize","Benin","Bhutan","Bolivia","Bosnia Herzegovina","Botswana","Brazil","Brunei","Bulgaria","Burkina","Burundi","Cambodia","Cameroon","Canada",
 "Cape Verde","Central African Rep","Chad","Chile","China","Colombia","Comoros","Congo","Democratic Republic of the Congo","Costa Rica","Croatia","Cuba","Cyprus","Czech Republic","Denmark",
-"Djibouti","Dominica",'Dominican Republic','East Timor','Ecuador','Egypt','El Salvador','Equatorial Guinea','Eritrea','Estonia','Ethiopia','Fiji','Finland','France','Gabon','Gambia',
+"Djibouti","Dominica",'Dominican Republic','East Timor','Ecuador','Egypt','El Salvador','Equatorial Guinea','Eritrea','Estonia','Ethiopia','Fiji','Finland','France', 'French Guiana','Gabon','Gambia',
 'Georgia','Germany','Ghana','Greenland', 'Greece','Grenada','Guatemala','Guinea','Guinea-Bissau','Guyana','Haiti','Honduras','Hungary','Iceland','India','Indonesia','Iran','Iraq','Republic of Ireland',
 'Israel','Italy','Ivory Coast','Jamaica','Japan','Jordan','Kazakhstan','Kenya','Kiribati','Korea North','Korea South','Kosovo','Kuwait','Kyrgyzstan','Laos','Latvia','Lebanon','Lesotho',
 'Liberia','Libya','Liechtenstein','Lithuania','Luxembourg','Macedonia','Madagascar','Malawi','Malaysia','Maldives','Mali','Malta','Marshall Islands','Mauritania','Mauritius','Mexico',
